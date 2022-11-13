cdf68547015fe970e511216df868032052e065dd
Marko Dupor <marko.dupor@gmail.com>
2022-11-13 09:19:08 +0100

    Added stub data, removed composer lock

---
+++ src/Database/Factories/DataMigrationFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\DataMigration;
+use Faker\Generator as Faker;
+
+$factory->define(DataMigration::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
